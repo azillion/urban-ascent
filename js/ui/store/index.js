1f9fd3e3b44dca839d743018676bf8da44c2bf32
alexander zillion <alex@alexzillion.com>
2023-06-19 12:41:29 -0500

    bevy init

--- js/ui/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import entities from './entities';
-
-export default configureStore({
-	reducer: entities,
-	devTools: true,
-});
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // .add_startup_system(setup.system())
+        .run();
+}
